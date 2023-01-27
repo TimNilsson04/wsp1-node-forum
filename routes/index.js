e2386362214796b559737e5bceecef5e6b38c1ae
TimNilsson04 <tim.nilsson2@elev.ga.ntig.se>
2023-01-27 11:17:08 +0100

    did alot of things

--- routes/index.js
+++ routes/index.js
@@ -20,13 +20,14 @@ router.get('/', async function (req, res, next) {
     });
 });
 
-
-
-router.post('/new', async function (req, res, next) {
-    const { author, title, content } = req.body;
-    const [rows] = await promisePool.query("INSERT INTO tn03forum (authorId, title, content) VALUES (?, ?, ?)", [author, title, content]);
-    res.redirect('/');
+router.get('/database', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM tn03forum");
+    res.render('database.njk', {
+        rows: rows,
+        title: 'Forum',
+    });
 });
+
 
 router.get('/new', async function (req, res, next) {
     const [users] = await promisePool.query("SELECT * FROM tn03users");
@@ -34,4 +35,25 @@ router.get('/new', async function (req, res, next) {
         title: 'Nytt inlägg',
         users,
     });
+});
+
+
+router.post('/new', async function (req, res, next) {
+    const { author, title, content } = req.body;
+
+    // Skapa en ny författare om den inte finns men du behöver kontrollera om användare finns!
+    let [user] = await promisePool.query('SELECT * FROM tn03users WHERE id = ?', [author]);
+    if (!user) {
+        user = await promisePool.query('INSERT INTO tn03users (name) VALUES (?)', [author]);
+    }
+
+    // user.insertId bör innehålla det nya ID:t för författaren
+
+    console.log(user[0])
+
+    const userId = user.insertId || user[0].id;
+
+    // kör frågan för att skapa ett nytt inlägg
+    const [rows] = await promisePool.query('INSERT INTO tn03forum (authorId, title, content) VALUES (?, ?, ?)', [userId, title, content]);
+    res.redirect('/'); // den här raden kan vara bra att kommentera ut för felsökning, du kan då använda tex. res.json({rows}) för att se vad som skickas tillbaka från databasen
 });
